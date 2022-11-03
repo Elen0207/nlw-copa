6da46ce740f355c5a0597358d5fb96e81b6d50da
Elen <elen.oliveira@finanzero.com.br>
2022-11-03 11:04:24 -0300

    second class

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 O projeto consiste em um bolão da copa do mundo de 2022.
 
 ![Web](https://user-images.githubusercontent.com/65963084/199237469-90a9b9d8-e899-4069-b267-26b3b39f5f1d.png)
+
+Comandos importantes:
+- `npx prisma migrate dev` - Para gerar a migrations
+- `npx prisma db seed` - Para popular o banco com os dados do arquivo **seed**
+- `npx prisma studio` - Para visualizar as tabelas no http://localhost:5556/
+- `npm install` - Para instalar as dependencias dos projetos(Rodar esse comando dentro de cada projeto)
+- `npm run dev` - Para rodar web e server(Deve estar nas respectivas pastas)
+- `npx expo start` - Para rodar o app(Deve estar dentro da pasta **mobile**)
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,27 +1,71 @@
-import Fastify from "fastify";
-import cors from "@fastify/cors";
-import { PrismaClient } from "@prisma/client";
+import Fastify from 'fastify'
+import cors from '@fastify/cors'
+import { PrismaClient } from '@prisma/client'
+import { z } from 'zod'
+import ShortUniqueId from 'short-unique-id'
 
 const prisma = new PrismaClient({
-  log: ['query']
-});
+	log: ['query']
+})
 
 async function bootstrap() {
-  const fastify = Fastify({
-    logger: true,
-  })
+	const fastify = Fastify({
+		logger: true
+	})
 
-  await fastify.register(cors, {
-    origin: true,
-  });
+	await fastify.register(cors, {
+		origin: true
+	})
 
-  fastify.get('/pools/count', async () => {
-    const count = await prisma.pool.count()
+	fastify.get('/pools/count', async () => {
+		const poolsCount = await prisma.pool.count()
 
-    return { count }
-  })
+		return { count: poolsCount  }
+	})
 
-  await fastify.listen({ port: 3333, host: '0.0.0.0' })
+	fastify.post('/pools', async (request, reply) => {
+		try {
+			const createPoolBody = z.object({
+				title: z.string({
+					required_error: 'O bolão deve ter um nome.',
+					invalid_type_error: 'O bolão deve ter um nome.'
+				}).min(1, {
+					message: 'O bolão deve ter um nome.'
+				})
+			})
+
+			const { title } = createPoolBody.parse(request.body)
+
+			const uniqueIdGenerate = new ShortUniqueId({ length: 6 })
+
+			const code = String(uniqueIdGenerate()).toUpperCase()
+
+			await prisma.pool.create({
+				data: {
+					title,
+					code
+				}
+			})
+
+			return reply.status(201).send(code)
+		} catch(e) {
+			return (e as Error).message
+		}
+	})
+
+	fastify.get('/users/count', async () => {
+		const usersCount = await prisma.user.count()
+
+		return { count: usersCount }
+	})
+
+	fastify.get('/guesses/count', async () => {
+		const guessesCount = await prisma.guess.count()
+
+		return { count: guessesCount }
+	})
+
+	await fastify.listen({ port: 3333, host: '0.0.0.0' })
 }
 
 bootstrap()
